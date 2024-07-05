978d8b5f9a67dd390c683e84515b77a937825130
PEDRO LUCAS SILVA <pedrol@devquixada.pinheiro.local>
2024-07-05 17:20:51 -0300

    preparando deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import autoprefixer from 'autoprefixer';
+import react from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    autoprefixer(),
-  ],
+  plugins: [react()],
+  base: "/konoha-library",
 });
