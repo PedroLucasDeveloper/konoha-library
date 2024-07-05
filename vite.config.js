b010ae564e97c3381bd09810e0b894a01ded66d1
PEDRO LUCAS SILVA <pedrol@devquixada.pinheiro.local>
2024-07-05 17:28:07 -0300

    iai deu certo?

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/konoha-library",
+  base: '/konoha-library/',
+  // Exemplo de configuração para excluir módulos problemáticos
+  optimizeDeps: {
+    exclude: ['@chakra-ui/*'],
+  },
 });
